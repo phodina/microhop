fb3978c1a1dc67c34b94ebe39ee8cfc14456ba84
Petr Hodina <petr.hodina@protonmail.com>
2026-01-23 22:44:16 +0100

    microhop: Add cmdline parser
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

---
+++ src/cmdline.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::Error;
+
+pub struct CmdLine {
+    params: HashMap<String, String>,
+}
+
+impl CmdLine {
+    pub fn new() -> Result<Self, Error> {
+        let cmdline = fs::read_to_string("/proc/cmdline")?;
+        let mut params = HashMap::new();
+
+        for param in cmdline.split_whitespace() {
+            if let Some((key, value)) = param.split_once('=') {
+                params.insert(key.to_string(), value.to_string());
+            } else {
+                params.insert(param.to_string(), String::new());
+            }
+        }
+
+        Ok(CmdLine { params })
+    }
+
+    pub fn get_root_device(&self) -> Option<&str> {
+        self.params.get("root").map(|s| s.as_str())
+    }
+
+    pub fn get_root_fstype(&self) -> Option<&str> {
+        self.params.get("rootfstype").map(|s| s.as_str())
+    }
+
+    pub fn get_root_options(&self) -> Option<&str> {
+        self.params.get("rootflags").map(|s| s.as_str())
+    }
+}
+
+impl Default for CmdLine {
+    fn default() -> Self {
+        Self::new().unwrap_or_else(|_| CmdLine { params: HashMap::new() })
+    }
+}
