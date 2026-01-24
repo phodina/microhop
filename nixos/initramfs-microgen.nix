a087899d934310dab3217e98a800012110cf449e
Petr Hodina <petr.hodina@protonmail.com>
2026-01-24 21:58:19 +0100

    nixos: Create initramfs for nixos
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

---
+++ nixos/initramfs-microgen.nix
@@ -0,0 +1,30 @@
+{ pkgs, kernel, microgen, microhop, microhopConfig }:
+
+pkgs.stdenv.mkDerivation {
+  name = "initramfs-microgen";
+
+  nativeBuildInputs = [ microgen pkgs.cpio pkgs.gzip ];
+
+  unpackPhase = "true";
+
+  buildPhase = ''
+    mkdir -p $TMPDIR/rootfs/lib/modules
+
+    cp -r ${kernel}/lib/modules/* $TMPDIR/rootfs/lib/modules/ || true
+
+    mkdir -p $out
+
+    ${microgen}/bin/microgen new \
+      --root $TMPDIR/rootfs \
+      --config ${microhopConfig} \
+      --kernel-config ${kernel.configfile} \
+      --filesystems squashfs,ext4 \
+      --block-devices virtio_blk,virtio_mmio \
+      --output $TMPDIR/build \
+      --file $out/initrd
+  '';
+  
+  installPhase = ''
+    ls -lh $out/initrd
+  '';
+}
