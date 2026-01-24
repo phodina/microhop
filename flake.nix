6536b24ed2ed78da7b52236c7963d92e45471f85
Petr Hodina <petr.hodina@protonmail.com>
2026-01-24 22:18:46 +0100

    flake: Add nixos QEMU example boot flow
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -10,6 +10,17 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        bootComponents = if system == "aarch64-linux" then {
+          kernel = import ./nixos/kernel.nix { inherit pkgs; };
+          u-boot = import ./nixos/u-boot.nix { inherit pkgs; };
+
+          microhopConfig = import ./nixos/microhop-config.nix { inherit pkgs; };
+
+          nixos-rootfs = import ./nixos/nixos-rootfs.nix {
+            inherit pkgs nixpkgs;
+          };
+        } else {};
       in
       {
         packages = {
@@ -88,19 +99,94 @@
           };
 
           default = self.packages.${system}.microgen;
-        };
+        } // (if system == "aarch64-linux" then {
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            make
-            cargo
-            rustc
-            rustfmt
-            clippy
-            pkg-config
-            util-linux
-          ];
-        };
+          kernel = bootComponents.kernel;
+          u-boot = bootComponents.u-boot;
+          nixos-rootfs = bootComponents.nixos-rootfs;
+
+          initramfs-microgen = import ./nixos/initramfs-microgen.nix {
+            inherit pkgs;
+            kernel = bootComponents.kernel;
+            microgen = self.packages.${system}.microgen;
+            microhop = self.packages.${system}.microhop;
+            microhopConfig = bootComponents.microhopConfig;
+          };
+
+          boot-overlayfs-musl = pkgs.writeScriptBin "boot-qemu-overlayfs-musl" ''
+            #!${pkgs.bash}/bin/bash
+
+            UBOOT="${bootComponents.u-boot}/u-boot.bin"
+            KERNEL="${bootComponents.kernel}/Image"
+            INITRD="${self.packages.${system}.initramfs-microgen}/initrd"
+            SQUASHFS="${bootComponents.nixos-rootfs}/rootfs.squashfs"
+
+            OVERLAY_IMG=$(mktemp -u /tmp/overlay-XXXXXX.img)
+            
+            ${pkgs.coreutils}/bin/truncate -s 512M "$OVERLAY_IMG"
+            ${pkgs.e2fsprogs}/bin/mkfs.ext4 -F -L overlay-storage "$OVERLAY_IMG"
+            
+            echo "=========================================="
+            echo "  Microhop Boot Flow"
+            echo "=========================================="
+            echo "Booting with musl-based rootfs and overlayfs..."
+            echo ""
+            echo "U-Boot:         $UBOOT"
+            echo "Kernel:         $KERNEL"
+            echo "Initrd:         $INITRD"
+            echo "Rootfs:         $SQUASHFS"
+            echo "Overlay:        $OVERLAY_IMG"
+            echo ""
+            echo "Boot flow: U-Boot -> Kernel -> Microhop initramfs -> NixOS musl rootfs"
+            echo "=========================================="
+            echo ""
+
+            ${pkgs.qemu}/bin/qemu-system-aarch64 \
+              -M virt \
+              -cpu cortex-a57 \
+              -m 1024M \
+              -smp 2 \
+              -bios "$UBOOT" \
+              -kernel "$KERNEL" \
+              -initrd "$INITRD" \
+              -drive file="$SQUASHFS",if=none,format=raw,readonly=on,id=hd0 \
+              -device virtio-blk-device,drive=hd0 \
+              -drive file="$OVERLAY_IMG",if=none,format=raw,id=hd1 \
+              -device virtio-blk-device,drive=hd1 \
+              -append "console=ttyAMA0" \
+              -nographic \
+              -no-reboot
+
+            rm -f "$OVERLAY_IMG"
+          '';
+        } else {});
+
+        apps = if system == "aarch64-linux" then {
+          boot-overlayfs-musl = {
+            type = "app";
+            program = "${self.packages.${system}.boot-overlayfs-musl}/bin/boot-qemu-overlayfs-musl";
+          };
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.boot-overlayfs-musl}/bin/boot-qemu-overlayfs-musl";
+          };
+        } else {};
+
+devShells.default = pkgs.mkShell {
+  buildInputs =
+    with pkgs;
+    [
+      gnumake
+      cargo
+      rustc
+      rustfmt
+      clippy
+      pkg-config
+    ]
+    ++ lib.optionals stdenv.isLinux [
+      util-linux
+    ];
+};
       }
     );
 }
